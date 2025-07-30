e88044823f97603f2f5b8da1e029f8d5068d7162
ArdenDiago <diagoarden@gmail.com>
2025-07-30 22:25:22 +0530

    Fixed the issue of the structure at the home page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,15 +20,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Fonts and icons */}
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
-        {/* Font Awesome Icons */}
-        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossOrigin="anonymous" async></script>
-        {/* Material Icons */}
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
+        />
       </head>
-      
-      {/* These classes on the body tag are CRITICAL for the theme's layout and styling */}
-      <body>
+      <body suppressHydrationWarning={true}>
         <StyledComponentsRegistry>
           <ThemeProvider>
             <TranslationProvider>
@@ -40,13 +39,11 @@ export default function RootLayout({
         </StyledComponentsRegistry>
 
         {/* Core JS Files */}
-        <Script src="/assets/js/core/popper.min.js" />
-        <Script src="/assets/js/core/bootstrap.min.js" />
-        <Script src="/assets/js/plugins/perfect-scrollbar.min.js" />
-        <Script src="/assets/js/plugins/smooth-scrollbar.min.js" />
-        
-        {/* Control Center for Material Dashboard: parallax effects, scripts for the example pages etc */}
-        <Script src="/assets/js/material-dashboard.min.js?v=3.1.0" />
+        <Script src="/assets/js/core/popper.min.js" strategy="lazyOnload" />
+        <Script src="/assets/js/core/bootstrap.min.js" strategy="lazyOnload" />
+        <Script src="/assets/js/plugins/perfect-scrollbar.min.js" strategy="lazyOnload" />
+        <Script src="/assets/js/plugins/smooth-scrollbar.min.js" strategy="lazyOnload" />
+        <Script src="/assets/js/material-dashboard.min.js?v=3.1.0" strategy="lazyOnload" />
       </body>
     </html>
   );
